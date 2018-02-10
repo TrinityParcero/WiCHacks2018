927404ea4a9c4fbe1e04cfc080e17218fa5e77d9
tgp6988 <tgp6988@IGM2000-17>
2018-02-10 15:53:15 -0500

    dino object and dino movement scripts version 1

---
+++ Dino.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//trinity parcero
+//dino object
+public class Dino : MonoBehaviour
+{
+
+    //fields
+    public string name;
+    //loc is held in int, basically index for
+    //accessing array of actual positions
+    public int loc;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        loc = 1; //start in middle row
+        name = "dino";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //sets a name for dino
+    public void SetName()
+    {
+        //stretch goal to have a "settable name"
+    }
+    
+}
---
+++ DinoMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//trinity parcero
+//controls dino movement and jumping
+//this goes on a dino object
+public class DinoMovement : MonoBehaviour
+{
+
+    //fields
+    private string rightCommand;
+    private string leftCommand;
+    private string jumpCommand;
+    public GameObject screenMan;
+
+    //set positions in inspector
+    public Vector3[] positions;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set commands based on dinos name
+        rightCommand = (name + ".MoveRight();");
+        leftCommand = (name + ".MoveLeft();");
+        jumpCommand = (name + ".Jump();");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetLoc();
+        Move();
+    }
+
+    //checks for input of commands
+    public void SetLoc()
+    {
+        //input is move right
+        if(screenMan.GetInput() == rightCommand)
+        {
+            //if on right edge, loop round to position 1
+            if(loc == 2)
+            {
+                loc = 0;
+            }
+            else
+            {
+                loc++;
+            }
+        }
+        //input is move left
+        else if(screenMan.GetInput() == leftCommand)
+        {
+            //if on left edge, loop round to position 3
+            if(loc == 0)
+            {
+                loc = 2;
+            }
+            else
+            {
+                loc--;
+            }
+        }
+        //input is jump
+        else if(screenMan.GetInput() == jumpCommand)
+        {
+            //if in lane 1, jump above that space
+            if(loc == 0)
+            {
+                loc = 3;
+            }
+            //and so on
+            if(loc == 1)
+            {
+                loc = 4;
+            }
+            //and so forth
+            if(loc == 2)
+            {
+                loc = 5;
+            }
+        }
+    }
+
+    //uses loc to set actual position
+    public void Move()
+    {
+        transform.position = positions[loc];
+    }
+    
+
+    
+
+   
+
+    
+}
